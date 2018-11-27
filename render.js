af8e97a0b81b59f2b37a7ebfb864e386919e3741
Idan Saban <idan23s.g@gmail.com>
2018-11-27 19:32:46 +0200

    applying style at the renderrer

--- render.js
+++ render.js
@@ -1,8 +1,8 @@
 const Renderer = () => {
     const renderComments = (comments) => {
-        let output = "<ul>";
+        let output = `<ul>`;
         for (let comment of comments) {
-            output += `<li data-id="${comment.id}"">${comment.text}</li>`;
+            output += `<li  class="comments" data-id="${comment.id}"">${comment.text} <div class="delete-comment">Remove Comment</div></li>`;
         }
         output += "</ul>";
         return output
@@ -12,7 +12,7 @@ const Renderer = () => {
         console.log("rendering");
         for (let post of posts) {
             console.log(`rendering: \t${post.text}`);
-            $("#posts").append(`<div data-id="${post.id}"><p>${post.text}</p> ${renderComments(post.comments)}</div>`);
+            $("#posts").append(`<div class="post" data-id="${post.id}"><p class="post-text">${post.text}</p> ${renderComments(post.comments)}<div class="delete">Remove Post</div></div>`);
         }
     }
     return {
