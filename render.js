cfadfd647b6c734bb4ac3f41774213e4d64e3bce
Idan Saban <idan23s.g@gmail.com>
2018-11-28 11:06:07 +0200

    applying style at the renderrer

--- render.js
+++ render.js
@@ -1,8 +1,9 @@
 const Renderer = () => {
     const renderComments = (comments) => {
-        let output = "<ul>";
+        let output = `<ul>`;
+        output+=`<input class="commentInput" type="text" placeholder="Comment...">`
         for (let comment of comments) {
-            output += `<li data-id="${comment.id}"">${comment.text}</li>`;
+            output += `<li  class="comments" data-id="${comment.id}"><i class="fas fa-trash-alt delete-comment"></i>${comment.text}</li>`;
         }
         output += "</ul>";
         return output
@@ -12,7 +13,7 @@ const Renderer = () => {
         console.log("rendering");
         for (let post of posts) {
             console.log(`rendering: \t${post.text}`);
-            $("#posts").append(`<div data-id="${post.id}"><p>${post.text}</p> ${renderComments(post.comments)}</div>`);
+            $("#posts").append(`<div class="post" data-id="${post.id}"><p class="post-text">${post.text}</p> ${renderComments(post.comments)}<div class="delete">Remove Post</div></div>`);
         }
     }
     return {
